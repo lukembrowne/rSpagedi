ab216687f2fd5175c2c798d8f3536f325a7de73f
Luke Browne <lukembrowne@gmail.com>
2015-02-11 11:54:22 -0600

    Added script to run spagedi from R console

--- R/run-spagedi.R
+++ R/run-spagedi.R
@@ -1,1 +1,28 @@
-run-spaged
+
+runSpagedi <- function(data_input, output_name){
+  
+  input <- paste("./out/", data_input , sep = "")
+  output <- output_name # Automatically adds './out/' path
+  
+  
+  options <- 
+    paste('"',
+          input,"\n", 
+        output, "\n", 
+        ifelse(file.exists(output), "e\n", ""), # If file already exists, overwrite
+        "\n", # Basic information menu
+        "1\n", # Choose individuals analysis
+        "1\n", # Choose Loiselle kinship coefficient
+        "3\n", # Make permutation tests
+        "\n", # Permutation options
+        "100\n", # Number of permutations 
+        "\n", # Output options
+        '"',
+      
+        sep = "")
+  
+  command <- paste("printf", options, "|", "spagedi")
+  
+  system(command)
+  
+}
--- R/workflow.R
+++ R/workflow.R
@@ -1,30 +1,40 @@
 library(readGenalex)
 
-source("./R/modify-genalex.R")
-source("./R/write-spagedi.R")
-source("./R/read-spagedi-output.R")
+source("./R/data-handling.R")
+source("./R/read-write-spagedi.R")
+source("./R/plot-spagedi.R")
+source("./R/run-spagedi.R")
 
 data <- readGenalex("./data/Genalex seedlings all 2 Feb 2015.txt")
 data <- groupPopsGenalex(data)
 
-seed <- subsetGenalexExtraCol(df = data, col_name = "Tissue", value = "Seed")
-leaf <- subsetGenalexExtraCol(df = data, col_name = "Tissue", value = "Leaf")
+beneath <- subsetGenalexPop(df = data, pop = "Lek")
+beneath_seed <- subsetGenalexExtraCol(df = beneath, col_name = "Tissue",
+                                      value = "Seed")
+beneath_leaf <- subsetGenalexExtraCol(df = beneath, col_name = "Tissue",
+                                      value = "Leaf")
 
-beneath <- subsetGenalexPop(df = data, pop = "Beneath")
-beneath_seed <- subsetGenalexExtraCol(df = beneath, col_name = "Tissue", value = "Seed")
-
-writeSpagedi(df = seed, file_name = "seed_spagedi.txt", num_dist_int = -5)
-writeSpagedi(df = leaf, file_name = "leaf_spagedi.txt", num_dist_int = -5)
 writeSpagedi(df = beneath_seed, file_name = "beneath_seed.txt", num_dist_int = -5)
+writeSpagedi(df = beneath_leaf, file_name = "beneath_leaf.txt", num_dist_int = -5)
+
+runSpagedi(data_input = "beneath_leaf.txt", output_name = "leaf.txt")
+runSpagedi(data_input = "beneath_seed.txt", output_name = "seed.txt")
+
+seed <- makeSpagediList("./out/seed.txt")
+leaf <- makeSpagediList("./out/leaf.txt")
+
+plotAutoCor(seed)
+plotAutoCor(leaf)
+
 
 ## Write function to run Spagedi through terminal in R for basic SGS analysis
 
 ## Need to make distance intervals custom in writeSpagedi
-
-## Write function that plots autocorrelation plots from read-spagedi-output 
 
 ## Write function that pulls apart maternal and paternal genotypes
 
 ## General debugging, adding in unit and error checking throughout functions
 
 ## Code to calculate Sp statistic
+
+## Get rid of intergroup clumping

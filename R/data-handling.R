16cc1ced4d699bea6db3e6daf88e6267a71352ec
Luke Browne <lukembrowne@gmail.com>
2015-02-12 08:50:34 -0600

    Moved GenAlex functions to separate file

--- R/data-handling.R
+++ R/data-handling.R
@@ -15,165 +15,46 @@ makeSpagediList <- function(path_to_out){
 }
 
 
-
-### Update attributes of a GenAlex dataframe
-
-updateAttributes <- function(df){
-  df$Plot <- factor(df$Plot)
-  attr(df, "n.samples") <- nrow(df) # N based on rows
-  attr(df, "n.pops") <- length(levels(factor(df$Plot)))
-  attr(df, "pop.labels") <- levels(factor(df$Plot))
-  new_pop_sizes <- as.numeric( table(df$Plot))
-  names(new_pop_sizes) <- levels(df$Plot)
-  attr(df, "pop.sizes") <- new_pop_sizes
-      # Update ploidy by counting # of columns after first two columns
-  if(ncol(df) == (attr(df, "n.loci") + 2)) {
-    attr(df, "ploidy") <- 1
-    attr(df, "locus.columns") <- 3:ncol(df)
-  }
-
-  return(df)
-}
-
-## Subset based on info in extra columns
-subsetGenalexExtraCol <- function(df, col_name, value){
-  
-  # Get row indices of subset
-  sub_ind <- which(attr(df, "extra.columns")[col_name] == value)
-  
-  # Create new dataframe that carries over attributes
-  new_df <- df[sub_ind, ]
-  attr(new_df, "extra.columns") <- attr(df, "extra.columns")[sub_ind, ]
-  
-  new_df <- updateAttributes(new_df)
-  return(new_df)
-  
-}
-
-# Subset based on pop
-subsetGenalexPop <- function(df, pop){
-  
-  # Get row indices of subset
-  sub_ind <- which(df$Plot == pop)
-  
-  # Create new dataframe that carries over attributes
-  new_df <- data[sub_ind, ]
-  attr(new_df, "extra.columns") <- attr(data, "extra.columns")[sub_ind, ]
-  
-  new_df <- updateAttributes(new_df)
-  return(new_df)
-  
-}
-
-# Group populations
-groupPopsGenalex <- function(df){
-  
-  df$Plot <- as.character(df$Plot)
-  df$Plot[grep(pattern = "Random", x = df$Plot)] <- "Random"
-  df$Plot[grep(pattern = "Lek", x = df$Plot)] <- "Lek"
-  df$Plot[df$Plot != "Random" & df$Plot != "Lek"] <- "Beneath"
-  df$Plot<- factor(df$Plot)  
-  return(df)
-}
-
-
-
-## Separating maternal and paternal genotypes
-
-
-separateMatPat <- function(genalex_df){
-
-
-    # Subset into 2 genalex DF based on tissue type
-    seed <- subsetGenalexExtraCol(df = genalex_df, col_name = "Tissue",
-                                          value = "Seed")
-    leaf <- subsetGenalexExtraCol(df = genalex_df, col_name = "Tissue",
-                                          value = "Leaf")
-    # initialize new dataframes that will hold paternal and maternal genotypes
-    mat <- seed
-    pat <- leaf
-
-    # Require that field numbers align perfectly
-    if(any(seed$Field_number != leaf$Field_number)) {
-      stop("Field numbers don't match up between leaf and seed samples!")}
-  
-    # Loop through genotypes, choosing which alleles are paternal or maternal
-    for(i in 1:nrow(seed)){ # Rows
-      
-      for(j in seq(3, ncol(seed), by = 2)){ # Every two columns
-        
-          # Save genotypes for each seed and leaf
-        seed_gen <- seed[i, c(j, j + 1)]
-        leaf_gen <- leaf[i, c(j, j + 1)]
-        
-          # If no genotypes match or there is missing data
-        if(sum(!(leaf_gen %in% seed_gen)) == 2){
-          mat[i, j] <- 0 # Set to missing data
-          pat[i, j] <- 0 # Set to missing data
-          if(0 %in% c(leaf_gen, seed_gen)) {warning("Missing Data!")
-          } else{
-          warning("Leaf and seed genotype mismatch!!")
-          }
-          next() # Jump to next individual if there's a mismatch
-        }
-        
-        # If both alleles match - aka both homozygotes     
-        if((sum(leaf_gen %in% seed_gen) == 2) & (sum(seed_gen %in% leaf_gen) == 2)){
-          #print("both homozygotes")
-          mat[i, j] <- leaf_gen[1]
-          pat[i, j] <- leaf_gen[1]
-          next()
-        }
-         
-        # If one allele in seed matches 2 in leaf
-        if((sum(leaf_gen %in% seed_gen) == 2) & (sum(seed_gen %in% leaf_gen) == 1)){
-          #print('leaf homozygous, seed not')
-          mat[i, j] <- leaf_gen[seed_gen %in% leaf_gen] # Switch order
-          pat[i, j] <- leaf_gen[1] # Doesn't matter - just take either allele
-        next()
-        }
-        
-          # If there is one allele that matches
-        if((sum(leaf_gen %in% seed_gen) == 1) & (sum(seed_gen %in% leaf_gen) == 1)){
-         #print("one allele match")
-         mat[i, j] <- leaf_gen[leaf_gen %in% seed_gen]
-         pat[i, j] <- leaf_gen[!(leaf_gen %in% seed_gen)]
-         next()
-        } 
-      } # j   
-    } # i
-
-  # Clean up maternal and paternal dataframes, taking out extra columns
-  mat_out <- mat[, c(1,2, seq(3, ncol(seed), by = 2))]
-  pat_out <- pat[, c(1,2, seq(3, ncol(seed), by = 2))]
-
-  # Copy over attributes
-  mat_names <- names(mat_out) # Could probably only use one line here
-  pat_names <- names(pat_out)
-
-  attributes(mat_out) <- attributes(mat)[-1] # Don't include names attr
-  attributes(pat_out) <- attributes(pat)[-1] # Don't include names attr
-
-  names(mat_out) <- mat_names
-  names(pat_out) <- pat_names
-
-  # Update attributes
-  mat_out <- updateAttributes(mat_out)
-  pat_out <- updateAttributes(pat_out)
-
-return(list(mat = mat_out, pat = pat_out))
-
-}
-
-
 ## Calculate sp statistic
 
 calcSp <- function(kin, row_name){
   return( ( - kin[row_name, "b-log(slope log dist)"]) / (1 - kin[row_name, 2]) )
 }
 
-SpSummary <- function(SpagediList)
-  {
+
+
+#' Sum of vector elements.
+#'
+#' \code{sum} returns the sum of all the values present in its arguments.
+#'
+#' This is a generic function: methods can be defined for it directly
+#' or via the \code{\link{Summary}} group generic. For this to work properly,
+#' the arguments \code{...} should be unnamed, and dispatch is on the
+#' first argument.
+#'
+#' @param ... Numeric, complex, or logical vectors.
+#' @param na.rm A logical scalar. Should missing values (including NaN)
+#'   be removed?
+#' @return If all inputs are integer and logical, then the output
+#'   will be an integer. If integer overflow
+#'   \url{http://en.wikipedia.org/wiki/Integer_overflow} occurs, the output
+#'   will be NA with a warning. Otherwise it will be a length-one numeric or
+#'   complex vector.
+#'
+#'   Zero-length vectors have sum 0 by definition. See
+#'   \url{http://en.wikipedia.org/wiki/Empty_sum} for more details.
+#' @examples
+#' sum(1:10)
+#' sum(1:5, 6:10)
+#' sum(F, F, F, T, T)
+#'
+#' sum(.Machine$integer.max, 1L)
+#' sum(.Machine$integer.max, 1)
+#'
+#' \dontrun{
+#' sum("a")
+#' }
+SpSummary <- function(SpagediList){
   kin <- SpagediList$kin
   mean <- calcSp(kin, "ALL LOCI")
   mean_jck <- calcSp(kin, "Mean")

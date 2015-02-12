7ab99beaef3b3c46808aeb3a9d2bd7122e643e16
Luke Browne <lukembrowne@gmail.com>
2015-02-12 09:58:58 -0600

    Update documentation

--- R/run-spagedi.R
+++ R/run-spagedi.R
@@ -1,18 +1,36 @@
-
-runSpagedi <- function(data_input, output_name, perm = 999){
+#' Execute SPAGeDi through R console.
+#'
+#' Calls SPAGeDi executable via system() function
+#'
+#' SPAGeDi must be installed on your system, and able to be run from the 
+#'  Shell/Terminal. Function is currently configured to run a basic spatial
+#'  autocorrelation analysis between individuals using the kinship coefficient
+#'  of Loiselle et al. 1995, testing for significance, and jacknifing over loci.
+#'
+#' @param input_name \(string\) - Name of SPAGeDi formatted input data file. Must be in 
+#'  working directory.
+#' @param output_name \(string\) - Name of output file. Will overwrite if file 
+#'  already exists, or create new file if it doesn't
+#' @param perm Number of permutations. Default is 999
+#' 
+#' @return Does not return anything directly, but writes results of analysis to
+#'  a tab-delimited text file at \code{output_name}. See \code{\link{readSpagediTable}}
+#'  for how to parse this output.
+#'
+#' @examples
+#' TODO
+#'
+runSpagedi <- function(input_name, output_name, perm = 999){
   
   if(perm < 40 | perm > 20000){ 
     stop("Number of permutations must be between 40 and 20000 ")}
   
-  input <- paste("./out/", data_input , sep = "")
-  output <- output_name # Automatically adds './out/' path
-  
-  
   options <- 
     paste('"',
-          input,"\n", 
-        output, "\n", 
-        ifelse(file.exists(paste("./out/", output, sep = "")), "e\n", ""), # If file already exists, overwrite
+          input_name,"\n", 
+        output_name, "\n", 
+        # If file already exists, overwrite
+        ifelse(file.exists(output_name), "e\n", ""), 
         "\n", # Basic information menu
         "1\n", # Choose individuals analysis
         "1\n", # Choose Loiselle kinship coefficient
@@ -27,5 +45,4 @@ runSpagedi <- function(data_input, output_name, perm = 999){
   command <- paste("printf", options, "|", "spagedi")
   
   system(command)
-  
 }

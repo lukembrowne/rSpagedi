ed4135c5957f80110f086f29d64cc7ec68f2915d
Luke Browne <lukembrowne@gmail.com>
2015-02-11 19:17:04 -0600

    Added function to summarize Sp statistic

--- R/data-handling.R
+++ R/data-handling.R
@@ -168,11 +168,28 @@ return(list(mat = mat_out, pat = pat_out))
 
 ## Calculate sp statistic
 
-calcSp <- function(SpagediList){
-  kin <- SpagediList$kin
-  all_loci <- (- kin[1, "b-log(slope log dist)"]) / (1 - kin[1, 2])
-  return(all_loci) 
+calcSp <- function(kin, row_name){
+  return( ( - kin[row_name, "b-log(slope log dist)"]) / (1 - kin[row_name, 2]) )
 }
+
+SpSummary <- function(SpagediList)
+  {
+  kin <- SpagediList$kin
+  mean <- calcSp(kin, "ALL LOCI")
+  mean_jck <- calcSp(kin, "Mean")
+  cat("\n\n")
+  cat("Mean Sp across all loci       == ", mean, "\n")
+  cat("-------\n")
+  cat("Mean Sp across jacknifed loci == ", mean_jck, "\n")
+  cat("-------\n")
+      # Select rows that have individual loci
+  loci_rows <- (grep("ALL LOCI", row.names(kin)) + 1):(grep("Jack", row.names(kin)) - 1)
+  sp_loci <- calcSp(kin, loci_rows )
+  names(sp_loci) <- row.names(kin)[loci_rows]
+  cat("Sp by loci\n\n")
+  return(sp_loci)
+}
+
 
 
 

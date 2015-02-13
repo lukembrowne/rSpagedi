b1373e32ddb07c924d4727fcf38449da5e2c93d0
Luke Browne <lukembrowne@gmail.com>
2015-02-13 16:25:11 -0600

    Add option to set max plotting distance on x axis

--- R/plot-spagedi.R
+++ R/plot-spagedi.R
@@ -12,7 +12,7 @@ plotAutoCor <- function(spagediList, overlay = FALSE, color = "black",
   obs <- as.numeric(perm["Obs val", ])
   conf_hi <- as.numeric(perm["95%CI-sup", ])
   conf_low <- as.numeric(perm["95%CI-inf", ])
-  max_dist <- as.numeric(dist["Max distance", ])
+  dist <- as.numeric(dist["Max distance", ])
   
     # Closed symbol if permutation was significant
   sig <- apply(perm[c(8,9,10), ], 2, FUN = function(x) {any(x < 0.05)})
@@ -21,21 +21,21 @@ plotAutoCor <- function(spagediList, overlay = FALSE, color = "black",
   
   if(overlay){
     par(new = TRUE)
-    points(max_dist, obs, type = "b", pch = symbols, xlab = "", ylab = "",
+    points(dist, obs, type = "b", pch = symbols, xlab = "", ylab = "",
            yaxt = "n", xaxt = "n", col = color, lwd = 2, lty = 1)
-    lines(max_dist, conf_hi, lty = 4, col = color)
-    lines(max_dist, conf_low, lty = 4, col = color)
+    lines(dist, conf_hi, lty = 4, col = color)
+    lines(dist, conf_low, lty = 4, col = color)
     
   } else{
   
-  plot(max_dist, obs, type = "b", pch = symbols, las = 1, 
+  plot(dist, obs, type = "b", pch = symbols, las = 1, 
        ylab = "Kinship", xlab = "Distance (m)", lwd = 2,
        ylim = c(min(obs, conf_low, na.rm = TRUE) * 1.1, 
                 max(obs, conf_hi, na.rm = TRUE) * 1.1),
        xlim = c(0, max_dist), col = color)
   abline(h = 0, lty = 1, col = "grey50")
-  lines(max_dist, conf_hi, lty = 4, col = color)
-  lines(max_dist, conf_low, lty = 4, col = color)
+  lines(dist, conf_hi, lty = 4, col = color)
+  lines(dist, conf_low, lty = 4, col = color)
   }
   
 }

7868386695cce2b2cca957dd7aeab724b918aa34
Luke Browne <lukembrowne@gmail.com>
2015-03-02 14:46:42 -0600

    Update example workflow

--- workflow.R
+++ workflow.R
@@ -1,51 +1,46 @@
 library(readGenalex)
+library(rSpagedi)
+source('~/Dropbox/Papers, Posters, and Talks/In Prep/Oenocarpus PSA/Oeno psa/analysis/SGS maternal vs paternal/genalex-misc 12 Feb 2015.R')
+setwd("~/Dropbox/Papers, Posters, and Talks/In Prep/Oenocarpus PSA/Oeno psa/analysis/SGS maternal vs paternal")
 
-
-data <- readGenalex("./data/Genalex seedlings all 2 Feb 2015.txt")
+data <- readGenalex::readGenalex("./PSA GenAlex All 2 Feb 2015.txt")
 data <- groupPopsGenalex(data)
 
-beneath <- subsetGenalexPop(df = data, pop = "Beneath")
-lek <- subsetGenalexPop(df = data, pop = "Lek")
-beneath_seed <- subsetGenalexExtraCol(df = beneath, col_name = "Tissue",
-                                      value = "Seed")
-beneath_leaf <- subsetGenalexExtraCol(df = beneath, col_name = "Tissue",
-                                      value = "Leaf")
+  # Adding jitter to spatial data to break up grouping
+attr(data, "extra.columns")$UTM1 <- jitter(as.numeric(attr(data, "extra.columns")$UTM1))
+attr(data, "extra.columns")$UTM2 <- jitter(as.numeric(attr(data, "extra.columns")$UTM2))
+
+beneath <- subsetGenalexPop(df = data, pop = "Random")
 
 ## Separating maternal and paternal
 mat <- separateMatPat(beneath)$mat
 pat <- separateMatPat(beneath)$pat
-writeSpagedi(mat, "mat.txt")
-writeSpagedi(pat, "pat.txt")
-runSpagedi(data_input = "mat.txt", output_name = "mat_out.txt")
-runSpagedi(data_input = "pat.txt", output_name = "pat_out.txt")
+writeSpagediGenAlex(mat, "mat.txt", dist_int = -10)
+writeSpagediGenAlex(pat, "pat.txt", dist_int = -10)
 
-mat_out <- makeSpagediList("./out/mat_out.txt")
-pat_out <- makeSpagediList("./out/pat_out.txt")
+runSpagedi(input_name = "mat.txt", output_name = "mat_out.txt", perm = 100)
+runSpagedi(input_name = "pat.txt", output_name = "pat_out.txt", perm = 100)
+
+
+mat_out <- makeSpagediList("mat_out.txt")
+pat_out <- makeSpagediList("pat_out.txt")
 
 plotAutoCor(mat_out)
-plotAutoCor(pat_out)
-calcSp(mat_out)
-calcSp(pat_out)
+plotAutoCor(pat_out, overlay = TRUE, color = "darkgreen")
 
+SpSummary(mat_out)
+SpSummary(pat_out)
 
+t.test(SpSummary(mat_out), SpSummary(pat_out), paired = T)
 
-writeSpagedi(df = beneath_seed, file_name = "beneath_seed.txt", num_dist_int = -5)
-writeSpagedi(df = beneath_leaf, file_name = "beneath_leaf.txt", num_dist_int = -5)
-
-runSpagedi(data_input = "beneath_leaf.txt", output_name = "leaf.txt")
-runSpagedi(data_input = "beneath_seed.txt", output_name = "seed.txt")
-
-seed <- makeSpagediList("./out/seed.txt")
-leaf <- makeSpagediList("./out/leaf.txt")
-
-
-
-## Need to make distance intervals custom in writeSpagedi
 
 ## General debugging, adding in unit and error checking throughout functions
 
-## Code to calculate Sp statistic
+# Integrate with hierfstat format so it's easy to calculate basic stats?
 
-## Get rid of intergroup clumping
+# Make readme for github
 
-## Add option to set number of permutations, make options more complex
+## Add documentation to each function - use cmd + shift + d to build documentation
+
+# Providing example data is a little complicated because we can't actually load the data, only point to it's path
+
